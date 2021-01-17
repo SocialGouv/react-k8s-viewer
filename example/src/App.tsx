280c3ebc52c1cd002a0aa65d0a15d3470741e22b
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-01-17 23:26:32 +0100

    progress

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,9 +1,15 @@
-import React, { VFC } from 'react';
+import React, { VFC } from "react";
 
-import { K8sViewer } from './reactComponentLib';
+import { K8sViewer } from "./reactComponentLib";
+
+import yaml from "yaml.macro";
+
+const manifests = yaml("./example.yaml");
+
+console.log("manifests", manifests);
 
 export const App: VFC = () => (
-  <div>
-    <K8sViewer />
+  <div style={{ width: "100%", height: 800, fontSize: "1.5em" }}>
+    <K8sViewer manifests={manifests} />
   </div>
 );
--- src/K8sViewer.tsx
+++ src/K8sViewer.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import ReactFlow from "react-flow-renderer";
 
-export interface K8sViewerProps {}
+import { makeLayout } from "./makeLayout";
+import { parseManifests } from "./parseManifests";
 
-export const K8sViewer: React.FC<K8sViewerProps> = ({}) => (
-  <div>Hello, world !</div>
-);
+export interface K8sViewerProps {
+  manifests: { kind: "List"; [U: string]: string } | undefined;
+}
+
+export const K8sViewer: React.FC<K8sViewerProps> = ({ manifests }) => {
+  const elements = parseManifests(manifests);
+  const flow = makeLayout(elements);
+  console.log({
+    manifests,
+    elements,
+    flow,
+  });
+  // return <div>io</div>;
+  return (
+    <ReactFlow
+      paneMoveable={true}
+      nodesDraggable={true}
+      elementsSelectable={false}
+      nodesConnectable={false}
+      elements={flow}
+    />
+  );
+};
