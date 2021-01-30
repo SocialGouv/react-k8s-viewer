318969aa80c1d27dce7687fcf27f678547c94681
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-01-30 23:45:07 +0100

    chore: extract component

--- src/component/K8sViewer.tsx
+++ src/component/K8sViewer.tsx
@@ -1,13 +1,10 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-// eslint-disable-next-line simple-import-sort/sort
 import React from "react";
-import ReactFlow, { Position, Handle } from "react-flow-renderer";
+import ReactFlow from "react-flow-renderer";
 import type { Elements, ReactFlowProps } from "react-flow-renderer";
 
-// eslint-disable-next-line import/no-unresolved
 import { makeLayout } from "../common/makeLayout";
-// eslint-disable-next-line import/no-unresolved
 import { parseManifests } from "../common/parseManifests";
+import { HtmlFlowNode } from "./HtmlFlowNode";
 
 type AnyObject = {
   [U: string]: any;
@@ -23,27 +20,6 @@ interface ManifestList {
 const onLoad = (reactFlowInstance: any) => {
   reactFlowInstance.fitView();
   console.log("onload");
-};
-
-interface HtmlLabelComponentProps {
-  data: AnyObject;
-}
-
-const HtmlLabelComponent = ({ data }: HtmlLabelComponentProps) => {
-  return (
-    <div
-      style={{
-        whiteSpace: "nowrap",
-        overflow: "hidden",
-      }}
-      className="react-flow__node-default react-flow__node-input react-flow__node-output"
-      title={data.label}
-    >
-      <Handle type="target" position={Position.Left} />
-      <div dangerouslySetInnerHTML={{ __html: data.label }} />
-      <Handle type="source" position={Position.Right} />
-    </div>
-  );
 };
 
 interface K8sViewerProps extends Omit<ReactFlowProps, "elements"> {
@@ -63,7 +39,7 @@ export const K8sViewer: React.FC<K8sViewerProps> = ({
   return (
     <React.Fragment>
       <ReactFlow
-        nodeTypes={{ ingress: HtmlLabelComponent }}
+        nodeTypes={{ ingress: HtmlFlowNode }}
         onLoad={onLoad}
         paneMoveable={true}
         nodesDraggable={true}
